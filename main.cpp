593b4b9b88e75b6abd229a65b0e39df1203fc4ee
Michael Schmidt <mike@Michaels-MBP.netgear.com>
2018-12-26 15:37:16 -0500

    added Genetic class starter code for future testing

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
 #include "globals.h"
 #include "greedy_local.h"
 #include "recursive.h"
+#include "genetic.h"
 
 int main() {
 
     Recursive* rec = new Recursive(18);
+    Genetic* gen = new Genetic(18);
 
     rec->solve();
     rec->print();
+
+    gen->solve();
+    gen->print();
+
+
 
     return 0;
 }
