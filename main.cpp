bcb2c0012452be5650beaef9d831e901d1f11c7d
Michael Schmidt <Michael.Schmidt82@gmail.com>
2019-01-09 10:56:23 -0600

    removed timing, debugging code.

--- main.cpp
+++ main.cpp
@@ -1,40 +1,40 @@
 #include "globals.h"
 #include "queens_base.h"
 #include "greedy_local.h"
-#include "recursive.h"
+#include "depth_first.h"
 #include "genetic.h"
 
 #include <chrono>
 
 int main(int argc, char* argv[]) {
 
-    assert(argc == 3 && "supply SEED and N");
-    srand(atoi(argv[1]));
+	//assert(argc == 3 && "supply SEED and N");
+	srand(atoi(argv[1]));
 
-    int N = atoi(argv[2]);
+	int N = atoi(argv[2]);
 
-    //QueensBase* recursive = new Recursive(N);
-    auto start = std::chrono::high_resolution_clock::now();
-    QueensBase* genetic = new Genetic(N);
-    //QueensBase* greedy = new GreedyLocal(N);
+	QueensBase* depth_first = new DepthFirstSearch(N);
+	//auto start = std::chrono::high_resolution_clock::now();
+	QueensBase* genetic = new GeneticSearch(N);
+	QueensBase* greedy = new GreedyLocal(N);
 
-    // cout << "Recursive:" << endl;
-    // recursive->solve();
-    // recursive->print();
-    // cout << endl;
+	cout << "Depth First Search:" << endl;
+	depth_first->solve();
+	depth_first->print();
+	cout << endl;
 
-    // cout << "Greedy Local:" << endl;
-    // greedy->solve();
-    // greedy->print();
-    // cout << endl;
+	cout << "Greedy Local Search:" << endl;
+	greedy->solve();
+	greedy->print();
+	cout << endl;
 
-    cout << "Genetic:" << endl;
-    genetic->solve();
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-    cout << "duration:" << duration.count() << endl;
-    genetic->print();
-    cout << endl;
+	cout << "Genetic Search:" << endl;
+	genetic->solve();
+	//auto stop = std::chrono::high_resolution_clock::now();
+	//auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+	//cout << "duration:" << duration.count() << endl;
+	genetic->print();
+	cout << endl;
 
-    return 0;
+	return 0;
 }
