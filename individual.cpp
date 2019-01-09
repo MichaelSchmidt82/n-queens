f9502d8bc9e839ee1699db7ee1400e3c1f582df2
Michael Schmidt <Michael.Schmidt82@gmail.com>
2019-01-09 10:57:21 -0600

    minor refactoring

--- individual.cpp
+++ individual.cpp
@@ -1,128 +1,126 @@
-#include "set_once.h"
 #include "individual.h"
 
 /* NOTE: this constructor may only be invoked once to set the static class variable N. */
 Individual::Individual(int _n) : N(_n) {
-    goal = (N * (N - 1)) / 2;
+	goal = (N * (N - 1)) / 2;
 
-    for (int i = 0; i < N; i++)
-        sequence.push_back(rand() % N);
+	for (int i = 0; i < N; i++)
+		sequence.push_back(rand() % N);
 
-    m_non_attacking = -1;
-    m_fitness = -1;
-    non_attacking();
+	m_non_attacking = -1;
+	m_fitness = -1;
+	non_attacking();
 }
 
 Individual::Individual(int _n, const Sequence & s) : N(_n) {
-    sequence = vector<Gene>(N);
+	sequence = vector<Gene>(N);
 
-    for (int i = 0; i < N; i++)
-        sequence[i] = s[i];
+	for (int i = 0; i < N; i++)
+		sequence[i] = s[i];
 }
 
 /* Form a child from two parents */
 Individual::Individual(int _n, const Individual & mother, const Individual & father)
-    : N(_n) {
-    int cutoff = rand() % N;
-    bool order = rand() % 2;
+	: N(_n) {
+	int cutoff = rand() % N;
+	bool order = rand() % 2;
 
-    sequence = Sequence(N);
+	sequence = Sequence(N);
+	goal = (N * (N - 1)) / 2;
 
-    if (cutoff == 0)
-        cutoff++;
-    else if (cutoff == N - 1)
-        cutoff--;
+	if (cutoff == 0)
+		cutoff++;
+	else if (cutoff == N - 1)
+		cutoff--;
 
-    if (order) {
-        for (int i = 0; i < cutoff; i++)
-            sequence[i] = mother[i];
-        for (int i = cutoff; i < N; i++)
-            sequence[i] = father[i];
-    } else {
-        for (int i = 0; i < cutoff; i++)
-            sequence[i] = father[i];
-        for (int i = cutoff; i < N; i++)
-            sequence[i] = mother[i];
-    }
+	if (order) {
+		for (int i = 0; i < cutoff; i++)
+			sequence[i] = mother[i];
+		for (int i = cutoff; i < N; i++)
+			sequence[i] = father[i];
+	}
+	else {
+		for (int i = 0; i < cutoff; i++)
+			sequence[i] = father[i];
+		for (int i = cutoff; i < N; i++)
+			sequence[i] = mother[i];
+	}
 
-    m_non_attacking = -1;
-    m_fitness = -1;
-    non_attacking();
+	m_non_attacking = -1;
+	m_fitness = -1;
+	non_attacking();
 }
 
 Individual::~Individual() {
-    sequence.clear();
+	sequence.clear();
 }
 
 bool Individual::operator> (const Individual & rhs) const {
-    return this->non_attacking() > rhs.non_attacking();
+	return this->non_attacking() > rhs.non_attacking();
 }
 
 bool Individual::operator== (const Individual & rhs) const {
-    bool same = true;
+	for (int i = 0; i < N; i++)
+		if ((*this)[i] != rhs[i])
+			return false;
 
-    for (int i = 0; i < N; i++)
-        if ((*this)[i] != rhs[i]) {
-            same = false;
-            break;
-        }
-
-    return same;
+	return true;
 }
 
 int Individual::operator[] (int i) const {
-    if (i >= 0 && i < N)
-        return sequence[i];
+	if (i >= 0 && i < N)
+		return sequence[i];
 
-    return -1;
+	return -1;
 }
 
 int Individual::non_attacking() const {
-    int attacks = 0;
+	int attacks = 0;
 
-    if (m_non_attacking == -1) {
-        /* Down */
-        for (int g = N - 1; g > 0; g--)
-            for (int c = g - 1; c >= 0; c--)
-                if (sequence[g] == sequence[c])
-                    attacks++;
-        /* Down & Left */
-        for (int row = 1; row < N; row++)
-            for (int col = sequence[row - 1] - 1, i = row; i < N && col >= 0; i++, col--)
-                if (sequence[i] == col)
-                    attacks++;
-        /* Down & Right */
-        for (int row = 1; row < N; row++)
-            for (int col = sequence[row - 1] + 1, i = row; i < N && col < N; i++, col++)
-                if (sequence[i] == col)
-                    attacks++;
+	if (m_non_attacking == -1) {
+		/* Down */
+		for (int g = N - 1; g > 0; g--)
+			for (int c = g - 1; c >= 0; c--)
+				if (sequence[g] == sequence[c])
+					attacks++;
+		/* Down & Left */
+		for (int row = 1; row < N; row++)
+			for (int col = sequence[row - 1] - 1, i = row; i < N && col >= 0; i++, col--)
+				if (sequence[i] == col)
+					attacks++;
+		/* Down & Right */
+		for (int row = 1; row < N; row++)
+			for (int col = sequence[row - 1] + 1, i = row; i < N && col < N; i++, col++)
+				if (sequence[i] == col)
+					attacks++;
 
-        m_non_attacking = goal - attacks;
-    }
-    return m_non_attacking;
+		m_non_attacking = goal - attacks;
+	}
+	return m_non_attacking;
 }
 
 double Individual::set_fitness(double sum, double start) {
-    if (m_fitness == -1)
-        m_fitness = start - (double(non_attacking()) / sum);
-    return m_fitness;
+	if (m_fitness == -1)
+		m_fitness = start - (double(non_attacking()) / sum);
+
+	return m_fitness;
 }
 
 void Individual::print() const {
-    for (int r = 0; r < N; r++) {
-        for (int c = 0; c < N; c++)
-            if (sequence[r] == c)
-                cout << "Q ";
-            else
-                cout << ". ";
-        cout << endl;
-    }
+	for (int r = 0; r < N; r++) {
+		for (int c = 0; c < N; c++)
+			if (sequence[r] == c)
+				cout << "Q ";
+			else
+				cout << ". ";
+		cout << endl;
+	}
 }
 
 void Individual::mutate() {
-    sequence[rand() % N] = rand() % N;
+	sequence[rand() % N] = rand() % N;
 
-    m_non_attacking = -1;
-    m_fitness = -1;
-    non_attacking();
+	m_non_attacking = -1;
+	m_fitness = -1;
+	non_attacking();
 }
--- individual.h
+++ individual.h
@@ -4,31 +4,31 @@
 #include "globals.h"
 
 class Individual {
-  public:
-    Individual(int n);
-    Individual(int n, const Sequence & s);
-    Individual(int n, const Individual & mother, const Individual & father);
-    ~Individual();
+public:
+	Individual(int n);
+	Individual(int n, const Sequence & s);
+	Individual(int n, const Individual & mother, const Individual & father);
+	~Individual();
 
-    int operator[] (int i) const;
-    bool operator> (const Individual & rhs) const;
-    bool operator== (const Individual & rhs) const;
+	int operator[] (int i) const;
+	bool operator> (const Individual & rhs) const;
+	bool operator== (const Individual & rhs) const;
 
-    int non_attacking() const;
-    bool solution() const { return goal == non_attacking(); }
-    double fitness () const { return m_fitness; }
-    void print() const;
+	int non_attacking() const;
+	bool solution() const { return goal == non_attacking(); }
+	double fitness() const { return m_fitness; }
+	void print() const;
 
-    double set_fitness (double sum, double start);
-    void mutate();
+	double set_fitness(double sum, double start);
+	void mutate();
 
-  private:
-    mutable int m_non_attacking;
-    mutable double m_fitness;
-    Sequence sequence;
+private:
+	mutable int m_non_attacking;
+	mutable double m_fitness;
+	Sequence sequence;
 
-    const int N;
-    int goal;
+	const int N;
+	int goal;
 };
 
 struct IndividualPtrCompare {
