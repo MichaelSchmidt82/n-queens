fec2965bd3262846436d8032483ba137e9c78dc6
Michael Schmidt <Michael.Schmidt82@gmail.com>
2019-01-09 10:54:33 -0600

    slight changes to algorithm.  attempting to improve performance still

--- genetic.cpp
+++ genetic.cpp
@@ -1,108 +1,113 @@
 #include "globals.h"
 #include "genetic.h"
 
-Genetic::~Genetic() {
-    for (IndividualPtr i : curr_gen)
-        delete i;
+GeneticSearch::~GeneticSearch() {
+	for (IndividualPtr i : curr_gen)
+		delete i;
 
-    for (IndividualPtr i : next_gen)
-        delete i;
+	for (IndividualPtr i : next_gen)
+		delete i;
 }
 
-void Genetic::solve() {
-    std::sort(curr_gen.begin(), curr_gen.end(), IndividualPtrCompare());
-    while (!curr_gen[0]->solution()) {
-        /* Examine and calculate fitness of current generation */
-        fitness(curr_gen);
+void GeneticSearch::solve() {
+	std::sort(curr_gen.begin(), curr_gen.end(), IndividualPtrCompare());
+	while (!curr_gen[0]->solution()) {
 
-        /* Create & Calculate the fitness of the next generation */
-        next_population();
-        fitness(next_gen);
-        inbreed_check();
-    }
+		/* Examine and calculate fitness of current generation */
+		fitness(curr_gen);
+
+		/* Create & Calculate the fitness of the next generation */
+		next_population();
+		fitness(next_gen);
+		inbreed_check();
+	}
 }
 
-void Genetic::print() const {
-    curr_gen[0]->print();
+void GeneticSearch::print() const {
+	curr_gen[0]->print();
 }
 
-void Genetic::init_population () {
-    curr_gen.reserve(pop_size);
-    for (int i = 0; i < pop_size; i++) {
-        curr_gen.push_back(new Individual(N));
-    }
+void GeneticSearch::init_population() {
+	curr_gen.reserve(pop_size);
+	for (int i = 0; i < pop_size; i++) {
+		curr_gen.push_back(new Individual(N));
+	}
 }
 
-void Genetic::next_population () {
-    /* Over reproduce */
-    next_gen.reserve(N * 2);
-    for (int i = pop_size * 2; i >= 0; i--)
-        next_gen.push_back(reproduce(curr_gen));
+void GeneticSearch::next_population() {
+	/* Over reproduce */
+	next_gen.reserve(N * 2);
+	for (int i = pop_size * 2; i > 0; i--)
+		next_gen.push_back(reproduce(curr_gen));
 
-    /* Survival of the fittest */
-    std::sort(next_gen.begin(), next_gen.end(), IndividualPtrCompare());
-    for (int i = (pop_size * 2); i > pop_size - 1 ; i--) {
-        delete next_gen[i];
-    }
 
-    next_gen.resize(N);
+
+	/* Survival of the fittest */
+	std::sort(next_gen.begin(), next_gen.end(), IndividualPtrCompare());
+	// fitness(next_gen);
+	for (int i = (pop_size * 2) - 1; i > pop_size - 1; i--) {
+		delete next_gen[i];
+	}
+	next_gen.resize(pop_size);
 }
 
-void Genetic::fitness(Population & population) {
-    double start = 1.0;
-    int sum = 0;
+void GeneticSearch::fitness(Population & population) {
+	double start = 1.0;
+	int sum = 0;
 
-    for (IndividualPtr i : population)
-        sum += i->non_attacking();
+	for (IndividualPtr i : population)
+		sum += i->non_attacking();
 
-    for (IndividualPtr i : population)
-        start = i->set_fitness(sum, start);
+	for (IndividualPtr i : population)
+		start = i->set_fitness(sum, start);
 }
 
-void Genetic::inbreed_check () {
+void GeneticSearch::inbreed_check() {
 
-    bool inbreed = *curr_gen[0] == *curr_gen[pop_size - 1];
+	bool inbreed = *curr_gen[0] == *curr_gen[pop_size - 1];
 
-    for (IndividualPtr i : curr_gen)
-        delete i;
+	for (IndividualPtr i : curr_gen)
+		delete i;
 
-    curr_gen.clear();
+	curr_gen.clear();
 
-    if (inbreed)
-        init_population();
-    else
-        curr_gen = next_gen;
-    next_gen.clear();
+	if (inbreed)
+		init_population();
+	else
+		curr_gen = move(next_gen);
+	next_gen.clear();
 }
 
-IndividualPtr Genetic::reproduce (const Population & population) {
-    IndividualPtr mother = population[0];
-    IndividualPtr father = population[0];
-    IndividualPtr child = nullptr;
+IndividualPtr GeneticSearch::reproduce(const Population & population) {
+	IndividualPtr mother = population[0];
+	IndividualPtr father = population[0];
+	IndividualPtr child = nullptr;
 
-    double threshold = fRand();
-    for (IndividualPtr i : population)
-        if (i->fitness() > threshold) {
-            mother = i;
-            break;
-        }
+	double threshold = fRand();
+	for (IndividualPtr i : population)
+		if (i->fitness() > threshold)
+			mother = i;
+		else
+			break;
 
-    threshold = fRand();
-    for (IndividualPtr i : population)
-        if (i->fitness() > threshold) {
-            father = i;
-            break;
-        }
+	do {
+		threshold = fRand();
+		for (IndividualPtr i : population)
+			if (i->fitness() > threshold)
+				father = i;
+			else
+				break;
+	} while (mother == father);
 
-    child = new Individual(N, *mother, *father);
+	child = new Individual(N, *mother, *father);
 
-    if (fRand() < 0.025)
-        child->mutate();
+	if (fRand() < 0.025)
+		child->mutate();
 
-    return child;
+	return child;
 }
 
-double Genetic::fRand(double fMin, double fMax) {
-    double f = (double)rand() / RAND_MAX;
-    return fMin + f * (fMax - fMin);
+double GeneticSearch::fRand(double fMin, double fMax) {
+	double f = (double)rand() / RAND_MAX;
+	return fMin + f * (fMax - fMin);
 }
--- genetic.h
+++ genetic.h
@@ -5,33 +5,32 @@
 #include "queens_base.h"
 #include "individual.h"
 
-class Genetic : public QueensBase {
+class GeneticSearch : public QueensBase {
 
 public:
-    Genetic(int _n) : N(_n) {
-        pairs = N * (N - 1) / 2;
-        pop_size = 500;
+	GeneticSearch(int _n) : N(_n) {
+		pairs = N * (N - 1) / 2;
+		pop_size = 10000;
 
-        init_population();
-    }
-    ~Genetic();
+		init_population();
+	}
+	~GeneticSearch();
 
-    void print() const;
-    void solve();
+	void print() const;
+	void solve();
 
 private:
-    const int N;
-    int pairs;
-    int pop_size;
-    Population curr_gen;
-    Population next_gen;
+	const int N;
+	int pairs;
+	int pop_size;
+	Population curr_gen;
+	Population next_gen;
 
-    /* Prototypes */
-    void init_population();
-    void next_population();
-    void fitness(Population &);
-    void inbreed_check ();
-    double fRand (double fMin = 0.0, double fMax = 1.0);
-    IndividualPtr reproduce(const Population &);
+	void init_population();
+	void next_population();
+	void fitness(Population &);
+	void inbreed_check();
+	double fRand(double fMin = 0.0, double fMax = 1.0);
+	IndividualPtr reproduce(const Population &);
 };
 #endif
